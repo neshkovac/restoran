27eb20f32d02f3f9b5c7f585c1cbb8ad7ec42b1a
neshkovac <lneskovac@hotmail.com>
2018-06-03 22:49:51 +0200

    JS added. Will update smoothness.

--- src/js/main.js
+++ src/js/main.js
@@ -84,3 +84,16 @@ window.addEventListener('load', function () {
 window.addEventListener('scroll', function() {
     onScroll();
 });
+
+
+// Active links JS
+
+$(function() {
+    $('li a').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        $this.closest('ul').find('.active').removeClass('active');
+        $this.parent().addClass('active');
+
+    });
+});
